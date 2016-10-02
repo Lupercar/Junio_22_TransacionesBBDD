28aa1634fb16eeed71f81882060dcd594a1e3ae8
Lupercar <hotolete@gmail.com>
2016-10-03 00:27:00 +0200

    Transacción en BBDD oracle11g

--- src/consola/App.java
+++ src/consola/App.java
@@ -3,6 +3,7 @@ package consola;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Savepoint;
 import java.sql.Statement;
@@ -28,16 +29,29 @@ public class App {
         	conexion = App.getConexion();
         	conexion.setAutoCommit(false); //quitamos el auto commit de la sesión
         	
-        	Statement comando = conexion.createStatement();
-        	String sql = "INSERT INTO CLIENTE(id,nombre) VALUES (5,'Cliente5')"; 
-        	comando.executeUpdate(sql); 
+//        	Statement comando = conexion.createStatement();
+//        	String sql = "INSERT INTO CLIENTE(id,nombre) VALUES (5,'Cliente5')"; 
+//        	comando.executeUpdate(sql); 
         	
 //        	conexion.rollback(); //deshace los cambios hasta el último commit
         	savepoint1 = conexion.setSavepoint(); 
         	
-        	comando.close();
+//        	comando.close();
+        	
+        	String sql2 = "INSERT INTO PEDIDO(id,fecha,info,idcliente) VALUES (5,?,'PedidoCliente5',5)"; 
+        	PreparedStatement comando2 = conexion.prepareStatement(sql2);
+        	comando2.setDate(1, new java.sql.Date(2015, 4, 30));
+        	
+        	comando2.execute();  
+        	
+        	comando2.close();
+        	
+        	conexion.commit(); //confirmamos
+        	conexion.close();
         }catch (SQLException ex) {
-        	conexion.rollback(savepoint1);
+        	if(savepoint1 != null)
+        		conexion.rollback(savepoint1);//Deshace cambios hasta savepoint
+        		conexion.commit();
 		}
 	}
 	
